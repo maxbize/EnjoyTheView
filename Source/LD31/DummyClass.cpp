97920d90e201f3e25e8a872f9c7f509054c8c8ec
Frenchie14 <mbize2005@gmail.com>
2014-12-11 01:30:27 -0800

    Initial Commit
    Initial commit of my Ludum Dare 48-hour game development challenge!

---
+++ Source/LD31/DummyClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LD31.h"
+#include "DummyClass.h"
+
+DummyClass::DummyClass()
+{
+}
+
+DummyClass::~DummyClass()
+{
+}
